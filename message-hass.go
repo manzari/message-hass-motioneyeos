f83e171f4a3fb2cb413a228144649f52b9fac92d
manzari <22736528+manzari@users.noreply.github.com>
2020-11-06 20:07:31 +0100

    apply code style

--- message-hass.go
+++ message-hass.go
@@ -22,14 +22,14 @@ type CameraConfig struct {
 }
 
 type MqttConfig struct {
-	Host              string
-	User              string
-	Pass              string
-	Dump              bool
-	Retain            bool
-	BaseTopic         string
-	AutoConfig        bool
-  Cameras           []CameraConfig
+	Host       string
+	User       string
+	Pass       string
+	Dump       bool
+	Retain     bool
+	BaseTopic  string
+	AutoConfig bool
+	Cameras    []CameraConfig
 }
 
 type ConfigMessage struct {
@@ -41,20 +41,20 @@ type ConfigMessage struct {
 func main() {
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		defaultConfig := MqttConfig{
-			Host:              "localhost:1883",
-			User:              "user",
-			Pass:              "70p53cr37",
-			Dump:              false,
-			Retain:            false,
-			BaseTopic:         "homeassistant",
-      Cameras: []CameraConfig{
-        {
-			    DeviceName:        "entrance_camera_motion",
-			    DeiceFriendlyName: "Entrance Motion",
-			    DeviceClass:       "motion",
-        },
-      },
-			AutoConfig:        true,
+			Host:      "localhost:1883",
+			User:      "user",
+			Pass:      "70p53cr37",
+			Dump:      false,
+			Retain:    false,
+			BaseTopic: "homeassistant",
+			Cameras: []CameraConfig{
+				{
+					DeviceName:        "entrance_camera_motion",
+					DeiceFriendlyName: "Entrance Motion",
+					DeviceClass:       "motion",
+				},
+			},
+			AutoConfig: true,
 		}
 		defaultConfigJson, _ := json.Marshal(defaultConfig)
 		err = ioutil.WriteFile(configPath, defaultConfigJson, 0600)
@@ -75,18 +75,18 @@ func main() {
 		os.Exit(1)
 	}
 
-  index := flag.Int("cam", 0, "Index of the camera in the config.  Defaults to the first camera")
+	index := flag.Int("cam", 0, "Index of the camera in the config.  Defaults to the first camera")
 
-  flag.Parse()
+	flag.Parse()
 	if flag.Arg(0) != "ON" && flag.Arg(0) != "OFF" {
 		fmt.Fprintln(os.Stderr, "usage: message-hass -cam=<INDEX> <ON/OFF>")
 		os.Exit(1)
 	}
 
-  if *index >= len(config.Cameras) {
-    fmt.Fprint(os.Stderr, "Failed to load camera in config file at index: ", *index)
-    os.Exit(1)
-  }
+	if *index >= len(config.Cameras) {
+		fmt.Fprint(os.Stderr, "Failed to load camera in config file at index: ", *index)
+		os.Exit(1)
+	}
 
 	conn, err := net.Dial("tcp", config.Host)
 	if err != nil {
@@ -102,7 +102,7 @@ func main() {
 	}
 	fmt.Println("Connected with client id", cc.ClientId)
 
-  camera := config.Cameras[*index]
+	camera := config.Cameras[*index]
 	stateTopic := config.BaseTopic + "/binary_sensor/" + camera.DeviceName + "/state"
 	if config.AutoConfig {
 		configTopic := config.BaseTopic + "/binary_sensor/" + camera.DeviceName + "/config"
